76c576a8a06f235ca5ab363d1161007bea139473
kgtkr <kgtkr.jp@gmail.com>
2018-09-13 23:00:48 +0900

    log

--- log.py
+++ log.py
@@ -9,19 +9,26 @@ try:
 except:
     pass
 
-api = lib.get_api()
-id = int(time.time()*1000)
-old_id = lib.old_id(id)
-if old_id != None:
-    old_data = lib.Data.load(old_id)
-    users = list(old_data.followers | old_data.friends | old_data.outgoing)
-    actives = []
-    for x in lib.list_split(100, users):
-        actives.extend([x.id for x in api.lookup_users(user_ids=x)])
-else:
-    actives = []
+while True:
+    try:
+        api = lib.get_api()
+        id = int(time.time()*1000)
+        old_id = lib.old_id(id)
+        if old_id != None:
+            old_data = lib.Data.load(old_id)
+            users = list(old_data.followers |
+                         old_data.friends | old_data.outgoing)
+            actives = []
+            for x in lib.list_split(100, users):
+                actives.extend([x.id for x in api.lookup_users(user_ids=x)])
+        else:
+            actives = []
 
-data = lib.Data(id=id, friends=list(tweepy.Cursor(api.friends_ids, count=5000).items()), followers=list(tweepy.Cursor(
-    api.followers_ids, count=5000).items()), outgoing=[int(x) for x in tweepy.Cursor(api.friendships_outgoing).items()], actives=actives)
+        data = lib.Data(id=id, friends=list(tweepy.Cursor(api.friends_ids, count=5000).items()), followers=list(tweepy.Cursor(
+            api.followers_ids, count=5000).items()), outgoing=[int(x) for x in tweepy.Cursor(api.friendships_outgoing).items()], actives=actives)
 
-data.save()
+        data.save()
+    except:
+        print("エラー")
+
+    time.sleep(60*60)
